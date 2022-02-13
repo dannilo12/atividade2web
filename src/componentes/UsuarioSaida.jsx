58e6546a17d970d7bd663b7ce01339aaf09a5170
danteking1@gmail.comclear <danteking1@gmail.com>
2022-02-13 17:19:24 -0300

    ok

--- src/componentes/UsuarioSaida.jsx
+++ src/componentes/UsuarioSaida.jsx
@@ -1,13 +1,2 @@
 import React, {Component} from "react";
 class UsuarioSaida extends Component{
-
-render(props){
-    return
-(
-    <div>
-        <p>Eu sou {this.props.name} sou {this.props.tipo}</p>
-    </div>
-)
-}
-
-}
